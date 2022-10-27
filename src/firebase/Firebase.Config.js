b8a32df27573fe4052ad13f28cb24f71c35c26af
Bachan-kubi <felixghagra@gmail.com>
2022-10-27 13:48:47 +0600

    git changed

---
+++ src/firebase/Firebase.Config.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnQ-6OIPshwynaYkIuhcqb8S6E5iVmUTI",
+  authDomain: "learning-platform-92834.firebaseapp.com",
+  projectId: "learning-platform-92834",
+  storageBucket: "learning-platform-92834.appspot.com",
+  messagingSenderId: "474455560960",
+  appId: "1:474455560960:web:a08d3ca12ff5f509ac19e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
